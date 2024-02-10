cfa32fc685e1a5f6b263cb7044f584e5872b0307
AkshatBujethiya <akshatmeena2711@gmail.com>
2024-02-11 00:21:08 +0530

    Add request form

--- controllers/addRequest.js
+++ controllers/addRequest.js
@@ -1,9 +1,42 @@
 const { Router } = require('express');
+const Request = require('../models/request');
 const addFormRoute = Router();
+var upload = require('../config/storage');
 
 addFormRoute.get('/addRequest', (req, res) => {
   user = req.params.user;
   res.render('requestform');
 });
 
+addFormRoute.post('/addRequest', upload.single('image'), (req, res) => {
+  data = req.body;
+  console.log(data);
+  if (!req.body.money) {
+    newRequest = new Request({
+      time: Date.now(),
+      title: data.title,
+      description: data.request_description,
+      email: req.user.email,
+      money_req: null,
+      secretary_status: false,
+      club_fa_status: false,
+      society_fa_status: false,
+      chairsap_status: false,
+    });
+    newRequest.save();
+  } else {
+    newRequest = new Request({
+      time: Date.now(),
+      title: data.title,
+      description: data.request_description,
+      email: req.user.email,
+      money_req: data.money,
+      secretary_status: false,
+      club_fa_status: false,
+      society_fa_status: false,
+      chairsap_status: false,
+    });
+    newRequest.save();
+  }
+});
 module.exports = addFormRoute;
--- models/request.js
+++ models/request.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema({
+  time: { type: Date },
   title: { type: String },
   description: { type: String },
   email: { type: String },
+  money_req: { type: String },
   secretary_status: { type: Boolean },
   club_fa_status: { type: Boolean },
   society_fa_status: { type: Boolean },
